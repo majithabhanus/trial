403a36150ab2fc552f853cfbd9cfc6eddcfb10ef
majima <majithabhanus@gmail.com>
2026-02-10 21:11:05 +0530

    Refactor script to install Jenkins and Docker
    Updated the script to install Jenkins and Docker, removed old commands, and added necessary repository keys.

--- INSTANCE/script.sh
+++ INSTANCE/script.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-  
-  set -eux
-exec > /var/log/user-data.log 2>&1
+sudo apt update
+sudo apt install -y wget gnupg software-properties-common
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository --yes https://packages.adoptium.net/artifactory/deb
+sudo apt update
+sudo apt install temurin-21-jdk -y
+/usr/bin/java --version
 
-# Wait for cloud-init to finish
-while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
-  sleep 2
-done
+#install jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
 
-apt update -y
-apt install -y openjdk-17-jre curl gnupg
-
-# Jenkins key
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \
-  | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-
-# Jenkins repo
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian binary/" \
-  | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-apt update -y
-apt install -y jenkins
-
-systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
+#install docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+sudo systemctl restart jenkins
+docker run -d --name sonar -p 9000:9000 sonarqube:community
